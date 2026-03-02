21415b570dc24752d8e5ba647212478f7f388f07
Udara Lakshan <ulakshan061@gmail.com>
2026-03-02 23:28:08 +0530

    complete

---
+++ part1/courseInfo/src/components/Total.jsx
@@ -0,0 +1,9 @@
+const Total = ({ parts }) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {parts.reduce((total, part) => total + part.exercises, 0)}
+    </p>
+  );
+};
+export default Total;
